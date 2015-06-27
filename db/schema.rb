21387d51b5d45ec7dcf66bced3b8f20c1db06377
Junya Okazaki <junya.okazaki@gmail.com>
2015-06-27 17:16:06 +0900

    review model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627073259) do
+ActiveRecord::Schema.define(version: 20150627081255) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "restaurant_name", null: false
@@ -21,6 +21,19 @@ ActiveRecord::Schema.define(version: 20150627073259) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "rate"
+    t.integer  "restaurant_id"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "reviews", ["restaurant_id", "user_id"], name: "index_reviews_on_restaurant_id_and_user_id", unique: true
+  add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
