83bcd24bf07b688032f3c28978a0d63bb51460b5
Junya Okazaki <junya.okazaki@gmail.com>
2015-06-28 16:55:34 +0900

    profile画像表示

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627123046) do
+ActiveRecord::Schema.define(version: 20150628065251) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "restaurant_name", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150627123046) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "profile_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
